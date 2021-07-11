626192dfd2cc352f853a5e189fbe248d11d18c7f
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2021-07-10 23:58:23 -0300

    refatoracao dos metodos

--- metodos.py
+++ metodos.py
@@ -1,4 +1,4 @@
-from controle_financeiro import movimentos
+# from controle_financeiro import movimentos
 from datetime import MINYEAR, date, datetime
 import dateutil.relativedelta
 
@@ -8,92 +8,68 @@ def exibirSaldoTotal(movimentos):
     total = 0
     for i in movimentos:
         total += i['valor']
-    print(total)
+    print(f'O saldo total é {total}')
 
-def exibirEntradas(movimentos, meses):
+
+def exibirEntradas(movimentos, meses = 1):
     """arg: movimentos = array com dicionarios,
        arg: meses = quantos meses anteriores à data atual serão considerados."""
     now = date.today()
-    menos = now + dateutil.relativedelta.relativedelta(months=-meses)
-
+    meses_considerados = now + dateutil.relativedelta.relativedelta(months=-meses)
     for i in movimentos:
-        if i['valor'] > 0 and menos < datetime.strptime(i['data'],'%Y-%m-%d').date():
-            print(i['data'])
-            print(i['valor'])
+        if i['valor'] > 0 and meses_considerados <= now: 
+            print(f"Movimentação de {i['valor']} em {i['data']}")
 
 
-def exibirSaidas(movimentos, meses):
+def exibirSaidas(movimentos, meses = 1):
     """arg: movimentos = array com dicionarios,
        arg: meses = quantos meses anterior a data atual."""
     now = date.today()
     menos = now + dateutil.relativedelta.relativedelta(months=-meses)
 
     for i in movimentos:
-        if i['valor'] < 0 and menos < datetime.strptime(i['data'],'%Y-%m-%d').date():
-            print(i['data'])
-            print(i['valor'])
-            #verificar erro após o retorno dos valores
+        if i['valor'] < 0 and menos < now:
+            print(f"Movimentação de {i['valor']} em {i['data']}")
 
 def exibirExtrato(movimentos):
     """arg: movimentos = array com dicionarios."""
     now = date.today()
-    while True:
-        print('''indique o periodo
-        (1) 7 dias  | (2) 15 dias  | (3) 6 meses  | (4) total
-          ''')
-        periodo = int(input())
-        if 0 < periodo < 5:
-            print(f"periodo {periodo}")
-            break
-    if(periodo == 1):
+    print('''indique o periodo
+    (1) 7 dias | (2) 15 dias  | (3) 6 meses  | (4) total (padrão)
+    ''')
+    periodo = int(input())
+    
+    menos = now
+
+    if (periodo == 1):
         menos = now + dateutil.relativedelta.relativedelta(days=-7)
-        for i in movimentos:
-            if menos < datetime.strptime(i['data'],'%Y-%m-%d').date():
-                if i['valor'] > 0:
-                    print('entradas')
-                    print(i['data'])
-                    print(i['valor'])
-                else:
-                    print('saidas')
-                    print(i['data'])
-                    print(i['valor'])
-
-    elif(periodo == 2):
+    elif (periodo ==2):
         menos = now + dateutil.relativedelta.relativedelta(days=-15)
-        for i in movimentos:
-            if menos < datetime.strptime(i['data'],'%Y-%m-%d').date():
-                if i['valor'] > 0:
-                    print('entradas')
-                    print(i['data'])
-                    print(i['valor'])
-                else:
-                    print('saidas')
-                    print(i['data'])
-                    print(i['valor'])
-
-    elif(periodo == 3):
+    elif (periodo ==3):
         menos = now + dateutil.relativedelta.relativedelta(months=-6)
-        for i in movimentos:
-            if menos < datetime.strptime(i['data'],'%Y-%m-%d').date():
-                if i['valor'] > 0:
-                    print('entradas')
-                    print(i['data'])
-                    print(i['valor'])
-                else:
-                    print('saidas')
-                    print(i['data'])
-                    print(i['valor'])
-
     else:
-        for i in movimentos:
-            if i['valor'] > 0:
-                print('entradas')
-                print(i['data'])
-                print(i['valor'])
-            else:
-                print('saidas')
-                print(i['data'])
-                print(i['valor'])
+        menos = date(1990,1,1)
+
+    print("""indique o tipo de visualização
+        (1) resumido (padrão) | (2) completo
+    """)
+
+    visualizacao = (int(input))
+
+    for i in movimentos:
+        print("oi")
+       ##Implementar
+        if menos <= date.now():#datetime.strptime(i['data'],'%Y-%m-%d').date():
+            print(f'Movimentação {} em {} ')
+            # if i['valor'] > 0:
+            #     print('entradas')
+            #     print(i['data'])
+            #     print(i['valor'])
+            # else:
+            #     print('saidas')
+            #     print(i['data'])
+            #     print(i['valor'])
+
     #verificar erros na comparação de data e melhorar saidas
 
 def novoLancamento(movimentos):
@@ -123,4 +99,3 @@ def editarLancamento(movimentos, id):
             i['inseridoPor'] = input('Digite seu nome ')
 
 
-help(exibirSaldoTotal)
